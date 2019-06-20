588bb15e3cfc6b389995c8e9a4b3c401d1e99055
Felix Martinez <felixaviermartinez@gmail.com>
2019-06-20 17:18:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Joe") {
+    console.log("The name is correct")
+} else {
+  console.log("The name is incorrect")
+}
+if (height === "74") {
+  console.log("The height is correct")
+} else {
+  console.log("The height is incorrect")
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
